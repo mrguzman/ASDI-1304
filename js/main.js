3d086788043feadc1d70141c8bf12d48bad129df
Juan Guzman <mrguzman83@fullsail.edu>
2013-04-18 17:41:27 -0700

    working with jquery

--- js/main.js
+++ js/main.js
@@ -4,29 +4,29 @@
 $('#form').on('pageinit', function(){
 	
 	//SAVE DATA to LocalStorage.
+		function saveAppt(){
 	
-	function saveAppt(key){
-		if (!key){
-			var id = Math.floor(Match.random()*100000001);
-		}else{
-			var id = key;
-		};
+		var id = Math.floor(Math.random()*100000001);
+		
 		var item = {};
-			item.fName = ["First Name:", $(fName).value];
-			item.lName = ["Last Name:", $(lName).value];
-			item.phoneNum = ["Phone Number:", $(phoneNum).value];
-			item.phoneType = ["Contact Type:", $(phoneType).value];
-			item.date = ["Appointment Date:", $(date).value];
-			item.time = ["Preferred Time:", $(time).value];
-			item.interest = ["Interest Level:", $(interest).value];
-			item.comments = ["Comments:", $(comments).value];
+			item.fName = ["First Name:", $("#fName").val()];
+			item.lName = ["Last Name:", $("#lName").val()];
+			item.phoneNum = ["Phone Number:", $("#phoneNum").val()];
+			item.phoneType = ["Contact Type:", $("#phoneType").val()];
+			item.date = ["Appointment Date:", $("#date").val()];
+			item.time = ["Preferred Time:", $("#time").val()];
+			item.interest = ["Interest Level:", $("#interest").val()];
+			item.comments = ["Comments:", $("#comments").val()];
 			localStorage.setItem(id, JSON.stringify(item));
 			
 			alert("Appointment Saved");
 			
+			console.log($("#saveButton").click(saveAppt));
+			
 	}
 	
+	$("#saveButton").click(saveAppt);
+
 	
-	
-	
+		
 }); 
